306e604353ebf875481c7dbb63822471fc1317c9
Adam Derbidge <adam9417@gmail.com>
2020-11-24 11:48:50 -0500

    First commit

---
+++ MixedMsgs.js
@@ -0,0 +1,7 @@
+//disc golf sayings
+function randomElement(array) {
+  
+}
+
+const opening = ['Daaang', 'Ask me how I', 'Completely', 'Absolutely', 'Nice', 'A tree ninja', 'Threaded', 'Shoulda'];
+const verbs = ['throw', 'thrown', 'huck', 'flick', 'shanked']
