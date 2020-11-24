effb0c95f41f58a232ec33bb591db093eb59184a
Adam Derbidge <adam9417@gmail.com>
2020-11-24 12:35:09 -0500

    Random element generator and arrays

--- MixedMsgs.js
+++ MixedMsgs.js
@@ -1,7 +1,16 @@
 //disc golf sayings
 function randomElement(array) {
-  
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
 
-const opening = ['Daaang', 'Ask me how I', 'Completely', 'Absolutely', 'Nice', 'A tree ninja', 'Threaded', 'Shoulda'];
-const verbs = ['throw', 'thrown', 'huck', 'flick', 'shanked']
+const opening = ['Daaang,', 'Ask me how I', 'Completely', 'Absolutely', 'A tree ninja', 'Shoulda'];
+const verbs = ['threw', 'hucked', 'flicked', 'shanked', 'threaded', 'pured', 'tagged'];
+const objects = ['my', 'your', 'that', 'a', 'the', 'this', 'that'];
+const nouns = ['disc', 'Thunderbird', 'Zone', 'gap', 'tree', 'Insanity'];
+
+function randomMessage() {
+  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns)].join(' ');
+};
+
+randomMessage(); 
