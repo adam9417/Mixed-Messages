4eb9a8efb0384d173f0686200c224f4a12866685
Adam Derbidge <adam9417@gmail.com>
2020-11-24 12:42:02 -0500

    Project complete

--- MixedMsgs.js
+++ MixedMsgs.js
@@ -1,16 +1,15 @@
-//disc golf sayings
-function randomElement(array) {
-  let randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-
 const opening = ['Daaang,', 'Ask me how I', 'Completely', 'Absolutely', 'A tree ninja', 'Shoulda'];
 const verbs = ['threw', 'hucked', 'flicked', 'shanked', 'threaded', 'pured', 'tagged'];
 const objects = ['my', 'your', 'that', 'a', 'the', 'this', 'that'];
 const nouns = ['disc', 'Thunderbird', 'Zone', 'gap', 'tree', 'Insanity'];
 
+function randomElement(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}; 
+
 function randomMessage() {
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns)].join(' ');
 };
 
-randomMessage(); 
+randomMessage();
